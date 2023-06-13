c23da4123801b8b1d20a789b1a88f65e259f0646
dakShh <daksh.dk207@gmail.com>
2023-06-13 15:44:09 +0530

    fixed footer responsiveness

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,9 @@ const Footer = () => {
     <footer className="bg-stone-800 text-accent-primary" data-aos="fade-up" data-aos-delay="200">
       <div className={clsx("container mx-auto ", "py-20 px-20")}>
         <div className={clsx("flex flex-col lg:flex-row w-full")}>
-          <div className="text-5xl mb-16 lg:mb-0 font-extrabold">Yaami Designs</div>
+          <div className="text-5xl mb-16 lg:mb-0 font-extrabold text-center md:text-left">
+            Yaami Designs
+          </div>
           <div className="w-full flex flex-col gap-y-6">
             <div className="mb-8">
               <div>
@@ -26,29 +28,30 @@ const Footer = () => {
               </div>
             </div>
             <div className={clsx("flex w-full items-center")}>
-              <div className="text-3xl font-extrabold w-[550px]">Start a conversation</div>
+              <div className="text-3xl md:mb-0 mb-6 font-extrabold w-full md:w-[320px] lg:w-[620px] text-center md:text-left">
+                Start a conversation
+              </div>
               <div className="border border-red-400 h-[1px] w-full hidden md:block"></div>
             </div>
             <div
               className={clsx(
-                "w-full flex flex-col md:flex-row justify-between",
-                "px-2",
-                "gap-y-5 md:gap-y-0"
+                "w-full flex flex-row justify-center md:justify-start gap-x-2",
+                "lg:px-2"
               )}
             >
-              <div className="flex gap-x-2 items-center">
-                <AiOutlineWhatsApp className="text-4xl" />
-                <div className="font-light text-accent-primary/80">+91-9769642166</div>
+              <div className="flex md:flex-row flex-col gap-x-2 gap-y-2 items-center">
+                <AiOutlineWhatsApp className="text-4xl " />
+                {/* <div className="font-light text-accent-primary/80">+91-9769642166</div> */}
               </div>
-              <div className="flex gap-x-2 items-center">
-                <AiOutlineFacebook className="text-4xl" />
-                <div className="font-light text-accent-primary/80">
+              <div className="flex gap-x-2 md:flex-row flex-col items-center">
+                <AiOutlineFacebook className="text-4xl " />
+                {/* <div className="font-light text-accent-primary/80">
                   www.facebook.com/yaami-designs
-                </div>
+                </div> */}
               </div>
-              <div className="flex gap-x-2 items-center">
-                <AiOutlineInstagram className="text-4xl" />
-                <div className="font-light text-accent-primary/80">@yaami_designs</div>
+              <div className="flex gap-x-2 md:flex-row flex-col items-center">
+                <AiOutlineInstagram className="text-4xl " />
+                {/* <div className="font-light text-accent-primary/80">@yaami_designs</div> */}
               </div>
             </div>
           </div>
