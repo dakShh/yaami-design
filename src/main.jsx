c0f79efaa18bd951b32829f68e881b00e7d65c34
dakShh <daksh.dk207@gmail.com>
2024-09-02 18:34:44 -0400

    added prettier config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import Layout from "./Components/Layout";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Layout from './Components/Layout'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Layout>
@@ -13,4 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </Layout>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
