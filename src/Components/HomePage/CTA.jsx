b789868983d27ed4a4556b8f90017a1f93a347ea
dakShh <daksh.dk207@gmail.com>
2024-09-04 19:42:15 -0400

    fixed the responsiveness of cta section

--- src/Components/HomePage/CTA.jsx
+++ src/Components/HomePage/CTA.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 
 // ** Third party imports
-// import clsx from 'clsx'
+import clsx from 'clsx'
 
 // import Wave1 from '../../assets/svgs/waves-1.svg'
 import PrimaryButton from '../Buttons/primary-button'
 const CTA = () => {
   return (
     <section className='bg-secondary '>
-      <div className='gap-8 items-center py-40 mx-auto max-w-screen-xl flex justify-between'>
+      <div
+        className={clsx(
+          'container mx-auto px-4',
+          'gap-8 sm:items-center py-24 sm:py-40 mx-auto flex md:justify-between flex-col sm:flex-row'
+        )}
+      >
         <div className='mt-4 md:mt-0 max-w-xl'>
-          <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-accent-primary '>
+          <h2 className='mb-4 text-3xl md:text-4xl tracking-tight font-extrabold text-white '>
             Customised services
           </h2>
-          <p className='mb-6 font-light text-accent-primary md:text-lg '>
+          <p
+            className={clsx(
+              'sm:mb-6 font-light text-white text-sm md:text-md lg:text-lg',
+              'max-w-sm md:max-w-md lg:max-w-full'
+            )}
+          >
             Can&apos;t find what you&apos;re looking for? We’ll design your home the way you like,
             keeping your needs and budget in mind.
           </p>
         </div>
-        <PrimaryButton content='Get a quote' />
+        <div>
+          <PrimaryButton content='Get a quote' />
+        </div>
       </div>
     </section>
   )
